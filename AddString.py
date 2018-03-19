854211f7a1beecf1bce4869a3adc348d3e26f4f6
Sokhun Seng <sososeng@gmail.com>
2018-03-19 18:37:32 -0500

    .

---
+++ AddString.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+
+        st1 = int(num1)
+        st2 = int(num2)
+
+        return str(st1 + st2)
